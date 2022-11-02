8569c3de0464431d2effdd21232253233f8875cc
Ashmyrasheed <ashmytr21@gmail.com>
2022-11-02 12:04:55 +0530

    first

---
+++ book.py
@@ -0,0 +1,41 @@
+while True:
+
+    print("select an option from the menu")
+
+    print("1 add book")
+
+    print("2 view all book")  
+
+    print("3 search a book")
+
+    print("4 update the book")    
+
+    print("5 delete a book")
+
+    print("6 exit")
+    
+    choice = int(input('enter an option:'))
+
+    if(choice==1):
+
+        print('book enter selected')
+
+    elif(choice==2):
+
+        print('view book')
+
+    elif(choice==3):
+
+        print('search book')
+
+    elif(choice==4):
+
+        print('update book')
+
+    elif(choice==5):
+
+        print('delete book')
+
+    elif(choice==6):
+
+        break
